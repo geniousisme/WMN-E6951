e18ebb0f90e4b04720a0286ace506b325ae79334
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-05-23 22:54:47 -0400

    Modify gitignore & Finish the google map for classification

--- find@CU/libraries/static/js/googlemap.js
+++ find@CU/libraries/static/js/googlemap.js
@@ -1,20 +1,35 @@
-var locations = [
-      ['Science and Engineering Library', 40.810055, -73.962005, 1],
-      ['Pupin Hall', 40.810050, -73.961360, 2],
-      ['Joe Caffe', 40.810263, -73.961835, 3],
-      ['Schapiro Center for Engineering and Physical Science Research', 40.809627, -73.960819, 4],
-      ['Davis Auditorium', 40.809563, -73.960734, 5],
-      ['Mudd Building', 40.809318, -73.959900, 6],
-      ['CS Department', 40.809011, -73.959790, 7],
-      ['Engineering Terrace Garage', 40.808877, -73.959949, 8],
-      ['Columbia Business School', 40.809005, -73.961248, 9],
-      ['Uris Hall', 40.808926, -73.961360, 10],
-      ['Butler Library', 40.806396, -73.963218, 11],
-      ['Lerner Hall', 40.806999, -73.964025, 12]
+    var locations = [
+        ['Science and Engineering Library', 40.810055, -73.962005],
+        ['Pupin Hall', 40.810050, -73.961360],
+        ['Joe Caffe', 40.810263, -73.961835],
+        ['Schapiro Center for Engineering and Physical Science Research', 40.809627, -73.960819],
+        ['Davis Auditorium', 40.809563, -73.960734],
+        ['Mudd Building', 40.809318, -73.959900],
+        ['CS Department', 40.809011, -73.959790],
+        ['Engineering Terrace Garage', 40.808877, -73.959949],
+        ['Columbia Business School', 40.809005, -73.961248],
+        ['Uris Hall', 40.808926, -73.961360],
+        ['Butler Library', 40.806396, -73.963218],
+        ['Lerner Hall', 40.806999, -73.964025]
     ];
 
+    var location_cord_hash = {
+        'Science and Engineering Library': [40.810055, -73.962005],
+        'Pupin Hall': [40.810050, -73.961360],
+        'Joe Caffe': [40.810263, -73.961835],
+        'Schapiro Center for Engineering and Physical Science Research': [40.809627, -73.960819],
+        'Davis Auditorium': [40.809563, -73.960734],
+        'Mudd Building': [40.809318, -73.959900],
+        'CS Department': [40.809011, -73.959790],
+        'Engineering Terrace Garage': [40.808877, -73.959949],
+        'Columbia Business School': [40.809005, -73.961248],
+        'Uris Hall': [40.808926, -73.961360],
+        'Butler Library': [40.806396, -73.963218],
+        'Lerner Hall': [40.806999, -73.964025]
+    };
+
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 18,
+      zoom: 16,
       center: new google.maps.LatLng(40.807544, -73.962583),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
